006c07df787853fa1442f538afd6975a40761071
Corey Keller <cakeller43@gmail.com>
2016-09-01 21:35:25 -0500

    part 2

--- Day One/AdventOne.ex
+++ Day One/AdventOne.ex
@@ -13,3 +13,23 @@ defmodule AdventOne do
         findFloor(tail, floor - 1)
     end
 end
+
+defmodule AdventOnePartTwo do
+    def findFloor(input), do: findFloor(input, 0, 0)
+
+    defp findFloor("", floor, position) do
+         position
+    end
+
+    defp findFloor(_, floor, position) when floor == -1 do
+         position
+    end
+
+    defp findFloor("(" <> tail, floor, position) do
+        findFloor(tail, floor + 1, position + 1)
+    end
+
+    defp findFloor(")" <> tail, floor, position) do
+        findFloor(tail, floor - 1, position + 1)
+    end
+end
